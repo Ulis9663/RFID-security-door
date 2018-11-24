0fa296a9e3ce6f3a23f8a2515b0c9e9acd886843
Ulis9663 <43912718+Ulis9663@users.noreply.github.com>
2018-11-24 10:54:18 -0500

    Add files via upload

---
+++ Casa_seguridad.ino
@@ -0,0 +1,149 @@
+// Include required libraries
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
+#include <Keypad.h>
+#include <Servo.h>
+#include <SPI.h>
+// Create instances
+LiquidCrystal lcd(35, 33, 31, 29, 27, 25);
+MFRC522 mfrc522(53, 10); // MFRC522 mfrc522(SS_PIN, RST_PIN)
+Servo sg90;
+// Initialize Pins for led's, servo and buzzer
+// Blue LED is connected to 5V
+constexpr uint8_t greenLed = 13;
+constexpr uint8_t redLed = 12;
+constexpr uint8_t servoPin =40;
+constexpr uint8_t buzzerPin = 11;
+char initial_password[4] = {'1', '2', '3', '4'};  // Variable to store initial password
+String tagUID = "30 61 84 A6";  // String to store UID of tag. Change it with your tag's UID
+char password[4];   // Variable to store users password
+boolean RFIDMode = true; // boolean to change modes
+char key_pressed = 0; // Variable to store incoming keys
+uint8_t i = 0;  // Variable used for counter
+// defining how many rows and columns our keypad have
+const byte rows = 4;
+const byte columns = 4;
+// Keypad pin map
+char hexaKeys[rows][columns] = {
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
+};
+// Initializing pins for keypad
+byte row_pins[rows] = {9, 8, 7, 6};
+byte column_pins[columns] = {5, 4, 3, 2};
+// Create instance for keypad
+Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
+void setup() {
+  // Arduino Pin configuration
+  pinMode(buzzerPin, OUTPUT);
+  pinMode(redLed, OUTPUT);
+  pinMode(greenLed, OUTPUT);
+  sg90.attach(servoPin);  //Declare pin 40 for servo
+  sg90.write(90); // Set initial position at 90 degrees
+  lcd.begin(16, 2);   // LCD screen
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  lcd.clear(); // Clear LCD screen
+}
+void loop() {
+  // System will first look for mode
+  if (RFIDMode == true) {
+    lcd.setCursor(0, 0);
+    lcd.print("Puerta Cerrada");
+    lcd.setCursor(0, 1);
+    lcd.print("Acerca tu Tag");
+    // Look for new cards
+    if ( ! mfrc522.PICC_IsNewCardPresent()) {
+      return;
+    }
+    // Select one of the cards
+    if ( ! mfrc522.PICC_ReadCardSerial()) {
+      return;
+    }
+    //Reading from the card
+    String tag = "";
+    for (byte j = 0; j < mfrc522.uid.size; j++)
+    {
+      tag.concat(String(mfrc522.uid.uidByte[j] < 0x10 ? " 0" : " "));
+      tag.concat(String(mfrc522.uid.uidByte[j], HEX));
+    }
+    tag.toUpperCase();
+    //Checking the card
+    if (tag.substring(1) == tagUID)
+    {
+      // If UID of tag is matched.
+      lcd.clear();
+      lcd.setCursor (0, 0);
+      lcd.print("Tag Correcta");
+      lcd.setCursor (0, 1);
+      lcd.print("Hola Sensei");
+      digitalWrite(greenLed, HIGH);
+      delay(3000);
+      digitalWrite(greenLed, LOW);
+      lcd.clear();
+      lcd.print("Ingresa la Pass:");
+      lcd.setCursor(0, 1);
+      RFIDMode = false; // Make RFID mode false
+    }
+    else
+    {
+      // If UID of tag is not matched.
+      lcd.clear();
+      lcd.setCursor(0, 0);
+      lcd.print("Tag Incorrecta");
+      lcd.setCursor(0, 1);
+      lcd.print("Largo de aqui");
+      digitalWrite(buzzerPin, HIGH);
+      digitalWrite(redLed, HIGH);
+      delay(3000);
+      digitalWrite(buzzerPin, LOW);
+      digitalWrite(redLed, LOW);
+      lcd.clear();
+    }
+  }
+  // If RFID mode is false, it will look for keys from keypad
+  if (RFIDMode == false) {
+    key_pressed = keypad_key.getKey(); // Storing keys
+    if (key_pressed)
+    {
+      password[i++] = key_pressed; // Storing in password variable
+      lcd.print("*");
+    }
+    if (i == 4) // If 4 keys are completed
+    {
+      delay(200);
+      if (!(strncmp(password, initial_password, 4))) // If password is matched
+      {
+        lcd.clear();
+        lcd.setCursor(0, 0);
+        lcd.print("Pass Correcta");
+        lcd.setCursor (0, 1);
+        lcd.print ("Adelante Sensei");
+        
+        sg90.write(180); // Door Opened
+        digitalWrite(greenLed, HIGH);
+        delay(3000);
+        digitalWrite(greenLed, LOW);
+        sg90.write(90); // Door Closed
+        lcd.clear();
+        i = 0;
+        RFIDMode = true; // Make RFID mode true
+      }
+      else    // If password is not matched
+      {
+        lcd.clear();
+        lcd.print("Pass Incorrecta");
+        digitalWrite(buzzerPin, HIGH);
+        digitalWrite(redLed, HIGH);
+        delay(3000);
+        digitalWrite(buzzerPin, LOW);
+        digitalWrite(redLed, LOW);
+        lcd.clear();
+        i = 0;
+        RFIDMode = true;  // Make RFID mode true
+      }
+    }
+  }
+}
